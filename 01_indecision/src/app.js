f51c6816da757dd9a47a1e5f494ceaec2203df3e
Nathan <nathanjwtx@nathanjwtx.io>
2019-09-04 08:49:47 -0500

    Working with React and ReactDOM via import not script tags

--- 01_indecision/src/app.js
+++ 01_indecision/src/app.js
@@ -1,13 +1,23 @@
-import subtract, { square, add } from "./utils.js";
+// import subtract, { square, add } from "./utils.js";
 // the default export isSenior can be called anything when importing because it is the default
-import isSenior, { isAdult, canDrink } from "./person.js";
+// import isSenior, { isAdult, canDrink } from "./person.js";
+// import validator from 'validator';
 
-console.log("app is running!!");
+// console.log(validator.isEmail("test@gmail.com"));
 
-console.log(square(4))
-console.log(add(8, 4));
-console.log(subtract(7, 3));
-console.log(isAdult(19));
-console.log(canDrink(25));
 
-console.log(isSenior(64));
+// console.log("app is running!!");
+
+// console.log(square(4))
+// console.log(add(8, 4));
+// console.log(subtract(7, 3));
+// console.log(isAdult(19));
+// console.log(canDrink(25));
+
+// console.log(isSenior(64));
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const template = React.createElement("p", {}, "testing 123");
+ReactDom.render(template, document.getElementById("app"));
