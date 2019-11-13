ae40c45dba162e34db779fc3fa6dbe840217d8df
Nathan <nathanjwtx@nathanjwtx.io>
2019-11-13 08:46:08 -0600

    Updated webpack.config and added intro routes

--- 02_expensify/src/app.js
+++ 02_expensify/src/app.js
@@ -4,10 +4,40 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+    <div>
+        This is dashboard comp
+    </div>
+);
+
+const CreateExpensePage = () => (
+    <div>
+        This is create page
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is the edit page
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is the help page
+    </div>
+);
+
+
 const routes = (
     <BrowserRouter>
-
+        <div>
+            <Route path={"/"} component={ExpenseDashboardPage} exact={true}/>
+            <Route path={"/create"} component={CreateExpensePage} />
+            <Route path={"/edit"} component={EditExpensePage} />
+            <Route path={"/help"} component={HelpPage} />
+        </div>
     </BrowserRouter>
 );
 
-ReactDOM.render(<p>hello</p>, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
